b420a1d473bb41d974e6c280222648085b5171dd
ItzGeno <106875305+MrGenoYT@users.noreply.github.com>
2025-03-11 23:01:26 +0530

    Delete models directory

--- models/PasswordResetToken.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PasswordResetTokenSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  token: { type: String, required: true },
-  expiresAt: { type: Date, required: true }
-});
-
-module.exports = mongoose.model('PasswordResetToken', PasswordResetTokenSchema);
